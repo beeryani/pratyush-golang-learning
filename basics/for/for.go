f453316cda932fc77ffc59d0afaed27fbc4cc5ff
Pratyush Gupta <guptapratyush98@gmail.com>
2023-03-11 04:33:37 +0530

    - add exercise 17

--- basics/for/for.go
+++ basics/for/for.go
@@ -1,13 +1,11 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
 func main() {
-	x, y := 3, 4
-	f := math.Sqrt(float64(x*x + y*y))
-	z := uint(f)
-	fmt.Println(x, y, z)
+	sum := 11
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+	fmt.Println(sum)
 }
